4842f5c38157103808426caf5485d24309c5d015
Argol <81041301+earagnarok@users.noreply.github.com>
2021-04-15 20:01:36 +0200

    Fixed issue where state was not persisted over table reload

--- saveposition.lua
+++ saveposition.lua
@@ -1,22 +1,45 @@
-function onLoad()
-    self.addContextMenuItem("Save position", savePosition)
-    position = {}
-    player = {}
+function onLoad(script_state)
+    state = JSON.decode(script_state)
+
+    if (state == nil) then
+      state = {
+        position = {},
+        player = {},
+        saved = false
+      }
+    end
+
+    if (state.saved == true) then
+      self.addContextMenuItem("Reveal position", moveToPosition)
+    else
+      self.addContextMenuItem("Save position", savePosition)
+    end
 end
 
 function savePosition(color)
-  position = self.getPosition()
-  player = color
+  state.position = self.getPosition()
+  state.player = color
+  state.saved = true
+
   self.clearContextMenu()
   self.addContextMenuItem("Reveal position", moveToPosition)
 end
 
 function moveToPosition(color)
-  if (player == color) then
-    self.setPositionSmooth(position, false, false)
+  if (state.player == color) then
+    self.setPositionSmooth(state.position, false, false)
+
+    state.position = {}
+    state.player = {}
+    state.saved = false
+
     self.clearContextMenu()
     self.addContextMenuItem("Save position", savePosition)
   else
-    print("Only " .. Player[color].steam_name .. " can reveal saved position!")
+    broadcastToAll("Only " .. state.player .. " player can reveal saved position!", {r=1, g=0, b=0})
   end
+end
+
+function onSave()
+  return JSON.encode(state)
 end
